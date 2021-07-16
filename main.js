e47997d72d0a90be464d9fe7ce79349b21b317d1
Joe Gakumo <joegakumo1@gmail.com>
2021-07-17 02:21:46 +0300

    added a control structure

--- main.js
+++ main.js
@@ -26,10 +26,10 @@ function myValidation(){
     }else if(year==="" || year<1900 || year>2100){
         alert("Year out of bound");
     } else{
-        alert(dob)
+        alert(dob+akanNames());
     }
     
-};
+
 
     // array decratation
     var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
@@ -42,22 +42,72 @@ function myValidation(){
     const MM=month;
     const DD=date;    
 
-    daysOTheOfWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+   function daysOTheOfWeek (){ 
+       let day;
+       return day=Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
+                }
 
 
     // function to return the names
 
      function akanNames(){
-        
+        let gender=document.getElementById("gender").value;
+        switch (gender){
+            // female name here
+            case "femaleNames":
+                if(daysOTheOfWeek[0]===femaleNames[0]){
+                    alert("Your Akan name  is "+femaleNames[0]);
+                    
+                }else if(daysOTheOfWeek[1]===femaleNames[1]){
+                    alert("Your Akan name is "+femaleNames[1]);
+                }else if(daysOTheOfWeek[2]===femaleNames[1]){
+                    alert("Your akan name is "+femaleNames[2]);
+                }else if(daysOTheOfWeek[3]===femaleNames[3]){
+                    alert("Your Akan name "+femaleNames[3]);
+                } else if(daysOTheOfWeek[4]===femaleNames[4]){
+                    alert("Your Akan name "+femaleNames[4]);
+                } else if(daysOTheOfWeek[5]===femaleNames[5]){
+                    alert("Your Akan name "+femaleNames[5]);
+                } else if(daysOTheOfWeek[6]===femaleNames[6]){
+                    alert("Your Akan name "+femaleNames[6]);
+                } else{
+                    alert("No match of Akan name");
+                }
+                break;
+            
+                // malenames here
+            case "maleNames":
+                if(daysOTheOfWeek[0]===maleNames[0]){
+                    alert("Your Akan name  is "+maleNames[0]);
+                    
+                }else if(daysOTheOfWeek[1]===maleNames[1]){
+                    alert("Your Akan name is "+maleNames[1]);
+                }else if(daysOTheOfWeek[2]===maleNames[1]){
+                    alert("Your akan name is "+maleNames[2]);
+                }else if(daysOTheOfWeek[3]===maleNames[3]){
+                    alert("Your Akan name "+maleNames[3]);
+                } else if(daysOTheOfWeek[4]===maleNames[4]){
+                    alert("Your Akan name "+maleNames[4]);
+                } else if(daysOTheOfWeek[5]===maleNames[5]){
+                    alert("Your Akan name "+maleNames[5]);
+                } else if(daysOTheOfWeek[6]===maleNames[6]){
+                    alert("Your Akan name "+maleNames[6]);
+                } else{
+                    alert("No match of Akan name");
+                }
+                break;
+                
+        }
+
+      
 
     };
-
-
-
-
-
   
-
+};
+function findName(){
+    day= daysOTheOfWeek();
+    akanNames();
+  }
 
 
 
